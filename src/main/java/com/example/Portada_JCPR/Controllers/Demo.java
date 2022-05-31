f62ac65fbaccb2b7dfe8990e60113aaae7af559d
Rodrigo-mark <61090857+Rodrigo-mark@users.noreply.github.com>
2022-05-31 13:04:13 -0500

    Update Demo.java

--- src/main/java/com/example/Portada_JCPR/Controllers/Demo.java
+++ src/main/java/com/example/Portada_JCPR/Controllers/Demo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Demo {
 	
-	@GetMapping("/")
+	@GetMapping("/Demo/Portada")
 	public String Portada()
 	{
 		return "Portada";
